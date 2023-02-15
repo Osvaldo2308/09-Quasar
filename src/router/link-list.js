bc2b7a99171756f6c40df838de543135b511e8cb
Osvaldo2308 <osvaldo245678@gmail.com>
2023-02-15 10:39:16 -0600

    first commit

---
+++ src/router/link-list.js
@@ -0,0 +1,20 @@
+export const linksList = [
+  {
+    title: "Typography",
+    caption: "Tipos de letras en Quasar",
+    icon: "las la-spell-check",
+    link: "typography",
+  },
+  {
+    title: "Flex",
+    caption: "Estilos con Flex",
+    icon: "las la-layer-group",
+    link: "flex",
+  },
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "las la-folder-open",
+    link: "https://quasar.dev",
+  },
+];
